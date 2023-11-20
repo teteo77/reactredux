d63c3ea791616856158e9f0e664bf462c46f3d49
teteocom@naver.com <teteocom@naver.com>
2023-11-21 02:08:21 +0900

    4 my-app2파일 삭제 추가 및 스토어 개념공부

--- my-app2/src/index.js
+++ my-app2/src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const initialState = {
+  task: '',
+  tasks: []
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function tasksReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'INPUT_TASK':
+      return {
+        ...state,
+        task: action.payload.task
+      };
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.payload.task])
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(tasksReducer);
+
+const inputTask = (task) => ({
+  type: 'INPUT_TASK',
+  payload: {
+    task
+  }
+});
+
+const addTask = (task) => ({
+  type: 'ADD_TASK',
+  payload: {
+    task
+  }
+});
+
+function TodoApp({ store }) {
+  const { task, tasks } = store.getState();
+  return (
+    <div>
+      <input type="text" onChange={(e) => store.dispatch(inputTask(e.target.value))} />
+      <input type="button" value="add" onClick={() => store.dispatch(addTask(task))} />
+      <ul>
+        {
+          tasks.map(function(item, i) {
+            return (
+              <li key={i}>{item}</li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+function renderApp(store) {
+  render(
+    <TodoApp store={store} />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(() => renderApp(store));
+renderApp(store);
---
+++ my-app2/src/index2.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+
+// 스토어의 초기 상태
+  // 5 P107
+  // 요소에서 입력값을 관리할 수 있게 스토어에 task라는 속성을 추가합니다
+const initialState = {
+  task: '',
+  tasks: []
+};
+
+// 1
+// 리듀서 첫번쨰 매게 변수에는 현재 상태를 나타내는 state를 전달
+// 초기 상태로 이전에 정의한 initialState를 지정 했습니다.
+// 두번쨰 매개변수에는 어떤 조작을 할지 나태는 액션 객체를 전달합니다.
+// task를 추가하기 위한 액션 객체 입니다.
+function tasksReducer(state = initialState, action) {
+
+
+  switch (action.type) {
+      // 5-2 P107
+      // type이 INPUT_TASK인 액션이 디스패치될 때 task 입력 값을 저장됩니다.
+
+    case 'INPUT_TASK':
+      return {
+        ...state,
+        task: action.payload.task
+      };
+// 2 P98
+// ADD_TASK라는 type을 가진 액션이 발행되면 현재 task 목록을 나타내는 task를 추가하면 될 것입니다.
+// 새로운 배열을 생성하는 concat
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.payload.task])
+      };
+    default:
+      return state;
+  }
+}
+
+// 3 P100
+// 리덕스는 createStore로 스토어를 생성합니다.
+// 이렇게 만들어진 스토어는 유일성을 갖게 됩니다.
+// 애플리케이션 전체의 상태를 집약해서 관리하는 것입니다.
+// 이렇게 만들어진 createStore함수에 tasksReducer를 지정하면 스토어를 생성할수 있습니다.
+//  ㄴ 이렇게 만든 스토어에는 다음과 같이 4개의 메서드가 들어 있습니다.
+//  ㄴ dispatch, subscribe, getStatus, replacerReducer
+//  ㄴ dispatch를 이용하여 액션을 발행 합니다.
+const store = createStore(tasksReducer);
+
+// 4 P107
+// 리액트를 사용해 브라우저에서 task를 입력하게 만들 것이므로 
+// 스토어에서 해당 입력을 관리하게 해야 합니다.
+// 일단 태스크 입력 전용 액션크리에이터를 다음과 같이 추가합니다.
+const inputTask = (task) => ({
+  type: 'INPUT_TASK',
+  payload: {
+    task
+  }
+});
+
+const addTask = (task) => ({
+  type: 'ADD_TASK',
+  payload: {
+    task
+  }
+});
+
+function TodoApp({ store }) {
+  const { task, tasks } = store.getState();
+  return (
+    <div>
+    {/* 5-3 P108 */}
+    {/* 텍스트 박스가 변경될 때마다 해당 내용이 스토어의 task로 관리되며, 버튼이 눌리면 스토어의 tasks에 태스크가 추가됩니다. */}
+    {/* 그리고 태스크 배열을 목록으로 출력합니다. */}
+    {/* 이어서 이러한 컴포넌트를 렌더링 하기 위한 함수를 만듭니다. */}
+
+      <input type="text" onChange={(e) => store.dispatch(inputTask(e.target.value))} />
+      <input type="button" value="add" onClick={() => store.dispatch(addTask(task))} />
+      <ul>
+        {
+          tasks.map(function(item, i) {
+            return (
+              <li key={i}>{item}</li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+// 5-4 P108
+// 이렇게 하면 root라는 id를 가진 요소 내부에 TodoApp컴포넌트가 렌더링 됩니다.
+// 이어서 스토어의 상태 변화에 따라 뷰를 변화시키는 처리를 작성합니다.
+// 스토어의 subscribe 메서드를 사용해 상태 변환 시점에 뷰를 렌더링하는 함수를 호출합니다.
+function renderApp(store) {
+  render(
+    <TodoApp store={store} />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(() => renderApp(store));
+renderApp(store);
