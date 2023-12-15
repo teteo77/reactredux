c48ce9c1958da6f3d4bf16960aec569f86f32e31
teteo77 <134153801+teteo77@users.noreply.github.com>
2023-12-15 15:05:32 +0900

    8 수정기능 추가,  취소기능 만들기

--- my-app2-practice/src/index4.js
+++ my-app2-practice/src/index4.js
@@ -79,12 +79,9 @@ const delete2 = (index) => ({
 function TodoApp({ store }) {
   const { task, tasks } = store.getState();
   const [isUpdate, setIsUpdate] = useState(false)
-    // 3-2useState(false)가 초기값이고 isUpdate 가지는 값이다.
+  // 3-2useState(false)가 초기값이고 isUpdate 가지는 값이다.
   // 상태값, 상태 업데이트 함수
   // 상태는 setIsUpdate소괄호 안에 넣어서 수정
-  
-  const [isUpdate2, setIsUpdate2]= useState(true)
-
   return (
     <div>
       <input type="text" onChange={(e) => store.dispatch(inputTask(e.target.value))} />
@@ -98,18 +95,9 @@ function TodoApp({ store }) {
             return (<>
                       <li key={i}>{item}</li>
                       <input type="button" value="delete" onClick={()=>store.dispatch(delete2(i))}></input>
-                      {/* 3-1 update버튼 생성 */}
-                      {isUpdate2 && <> 
-                        <button value="update" onClick={() => {setIsUpdate(!isUpdate); setIsUpdate2(!isUpdate2)}}>update</button>
-                        {/* <button value="update" onClick={setIsUpdate(!isUpdate)}>update</button> */}
-                                              {/* ㄴ3-6 */}
-                      </>}
-                                            {/* 3-4 ㄴ 상태는 setIsUpdate(!isUpdate) 안에 넣어서 수정  */}
-                                            {/* 3-5 함수를 두개 넣을때는 중괄호를 넣고 선언해주기, 콜백함수이다 공부하기 */}
-                                            {/* 3-6 어떤 함수가 실행을 하고 나서 내부적으로 return문을 가지고 있거나 화살표 ㅎ함수처럼 중괄호가 생략되어서 실행딘 경우에는 반환 값을 가지게 되는데 리턴을문을 가지지않고 함수를 호출하게 되면
-                                            무조건 언드파인드가 반환이 된다. */}
-                                            
-
+                      {/* 3-1 */}
+                      <button value="update" onClick={() => setIsUpdate(!isUpdate)}>update</button>
+                                                                  {/* 3-4 ㄴ 상태는 setIsUpdate(!isUpdate) 안에 넣어서 수정  */}
                       {isUpdate && <> 
                       {/* 3-3isUpdate는 현제 false이고 &&연산자에 의해 두가지 값이 트루여야 표시됨 */}
                          <input type="text"></input>
